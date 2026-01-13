5c8d4e4f354aa8e2ba8d7106bffe334b0a47d3bc
Balaji <balaahir007@gmail.com>
2026-01-13 10:27:23 +0530

    codesift project setup done

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="bg-white dark:bg-black text-black dark:text-white p-4">
+        Dark mode test
+      </div>
+    </div>
+  );
+}
