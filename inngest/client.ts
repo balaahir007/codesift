9c06c4b6541f84fab8cd5af5a39d3745c054728c
Balaji <balaahir007@gmail.com>
2026-01-21 16:02:26 +0530

    06: error tracking

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,6 +1,9 @@
 import {Inngest} from 'inngest';
+
+import {sentryMiddleware} from '@inngest/middleware-sentry'
 export const inngest = new Inngest({
   id : 'codesift',
+  middleware : [sentryMiddleware()]
   
   // You can add more configuration options here if needed
   
